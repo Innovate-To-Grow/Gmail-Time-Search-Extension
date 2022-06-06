ef976d5e7e67b8d29cf81fc47716dca7f933e823
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-06-06 10:45:26 -0700

    added more functionality

--- scripts.js
+++ scripts.js
@@ -1,21 +1,90 @@
 $( function() {
     var formatted_date = "";
+    var formatted_from = "";
+    var formatted_to = "";
+    var formatted_cc = "";
+    var formatted_subject = "";
+    var formatted_label = "";
+    var formatted_attachment = "";
     flatpickr(".beforeDate");
     flatpickr(".afterDate");
     
     $("#addrange").on("click", function(){
         var before_date_text = document.getElementsByClassName("beforeDate")[0].value; //YYYY-MM-DD.
         var after_date_text = document.getElementsByClassName("afterDate")[0].value; //YYYY-MM-DD.
+        var from = document.getElementsByClassName("from")[0].value; //email@gmail.com
+        var to = document.getElementsByClassName("to")[0].value; //email@gmail.com
+        var cc = document.getElementsByClassName("ccinput")[0].value; //email@gmail.com
+        var subject = document.getElementsByClassName("subinput")[0].value; //Subject
+        var label = document.getElementsByClassName("labinput")[0].value; //Label
+        var attachment = document.getElementById("attachment").value; //true or false
         
-        if (before_date_text != "" && after_date_text  != ""){
-            formatted_date = "(after:" + after_date_text + " before:"+ before_date_text + ")";
-            document.getElementById("savedDate").innerHTML += "<p>" + formatted_date + "</p>";
+        var str1 = "(";
+        var str2 = ")";
+        
+        if(before_date_text != "" && after_date_text == ""){ // if before date is filled out but not after date
+            formatted_date = "before:" + before_date_text;
             document.getElementsByClassName("beforeDate")[0].value = "";
             document.getElementsByClassName("afterDate")[0].value = "";
-        }else{
-            alert("input box is empty");
+        }else if(before_date_text == "" && after_date_text != ""){ // if after date is filled out but not before date
+            formatted_date = "after:" + after_date_text;
+            document.getElementsByClassName("beforeDate")[0].value = "";
+            document.getElementsByClassName("afterDate")[0].value = "";
+        }else if(before_date_text != "" && after_date_text != ""){ // if both date is filled out
+            formatted_date = "after:" + after_date_text + " before:"+ before_date_text;
+            document.getElementsByClassName("beforeDate")[0].value = "";
+            document.getElementsByClassName("afterDate")[0].value = "";
         }
-        
+
+        if(from != ""){
+            formatted_from = "from:" + from;
+            document.getElementsByClassName("from")[0].value = "";
+        }
+        if(to != ""){
+            formatted_to = "to:" + to;
+            document.getElementsByClassName("to")[0].value = "";
+        }
+        if(cc != ""){
+            formatted_cc = "cc:" + cc;
+            console.log(document.getElementById("cc").checked);
+            document.getElementById("cc").checked = false;
+            document.getElementsByClassName("ccinput")[0].value = "";
+            $('input[name="ccinput"]').hide();
+        }
+        if(subject != ""){
+            formatted_subject = "subject:" + subject;
+            document.getElementById("subject").checked = false;
+            document.getElementsByClassName("subinput")[0].value = "";
+            $('input[name="subinput"]').hide();
+        }
+        if(label != ""){
+            formatted_label = "label:" + label;
+            document.getElementById("label").checked = false;
+            document.getElementsByClassName("labinput")[0].value = "";
+            $('input[name="labinput"]').hide();
+        }
+        if(attachment == 'true'){
+            formatted_attachment = "has:attachment";
+            document.getElementById("attachment").checked = false;
+            document.getElementById("attachment").value = 'false';
+        }
+
+        if(formatted_date != "" || formatted_from != "" || formatted_to != "" || formatted_cc != "" || formatted_subject != "" || formatted_label != "" || formatted_attachment != ""){
+            var final_format = str1 + formatted_date + " " + formatted_from + " " + formatted_to + " " + formatted_cc + " " + formatted_subject + " " + formatted_label + " " + formatted_attachment + str2;
+            document.getElementById("savedDate").innerHTML += "<p>" + final_format + "</p>";
+            // formatted_date, formatted_to, formatted_cc, formatted_subject, formatted_label = "";
+            formatted_date = "";
+            formatted_from = "";
+            formatted_to = "";
+            formatted_cc = "";
+            formatted_subject = "";
+            formatted_label = "";
+            formatted_attachment = "";
+
+            checkList.classList.remove('visible');
+        }else{
+            alert("Input is empty.");
+        }
     });
 
     $("#copybutton").on("click", function(){
@@ -67,6 +136,13 @@ $( function() {
             $('input[name="labinput"]').fadeIn();
         } else {
             $('input[name="labinput"]').hide();
+        }
+    });
+    $('input[name="attachment"]').on('change', function() {
+        if ($(this).is(':checked')) {
+          $(this).attr('value', 'true');
+        } else {
+          $(this).attr('value', 'false');
         }
     });
 
